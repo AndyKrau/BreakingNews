10b0b3a49d0056de630be9b03858c63de71b7d3e
AndyKrau <andrey.krause@yandex.ru>
2023-06-12 18:26:34 +0700

    fix: refactor create user code

--- BreakingNewsWeb/Services/CreateUser.cs
+++ BreakingNewsWeb/Services/CreateUser.cs
@@ -7,38 +7,54 @@ namespace BreakingNewsWeb.Services
 {
     public class CreateUser : ICreateUser
     {
-        UsersContext db;
+        readonly UsersContext db;
         public CreateUser(UsersContext usersContext)
         {
             db = usersContext;
         }
         public User CreateNewUser(User user)
         {
+            User newUser = user;
             var existUserName = db.users.FirstOrDefault(x=> x.Name == user.Name);
-            // надо разобараться тут! 
-            try
+            var existUserEmail = db.users.FirstOrDefault(e=> e.Email == user.Email);
+
+            if (existUserName == null)
             {
-                User newUser = new()
+                if (existUserEmail == null)
                 {
-                    Name = user.Name,
-                    Password = BCrypt.Net.BCrypt.HashPassword(user.Password),
-                    Email = user.Email,
-                    Country = user.Country,
-                    PhoneNumber = user.PhoneNumber,
-                    PostalCode = user.PostalCode,
-                };
-
-                db.Add(newUser);
-                db.SaveChanges();
-
-                return newUser;
+                    newUser.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+                    db.users.Add(newUser);
+                    db.SaveChanges();
+                    
+                    return newUser;
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
-
             return null;
+
+            // надо разобараться тут! 
+            //try
+            //{
+            //    User newUser = new()
+            //    {
+            //        Name = user.Name,
+            //        Password = BCrypt.Net.BCrypt.HashPassword(user.Password),
+            //        Email = user.Email,
+            //        Country = user.Country,
+            //        PhoneNumber = user.PhoneNumber,
+            //        PostalCode = user.PostalCode,
+            //    };
+
+            //    db.Add(newUser);
+            //    db.SaveChanges();
+
+            //    return newUser;
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
+
+
 
         }
     }
